65581536d2af23957fef13b884403c0227b68d5f
Boris Schapira <bschapira@clever-age.com>
2014-12-10 10:30:04 +0100

    Pas de suivi du lien

--- assets/fiche.js
+++ assets/fiche.js
@@ -1,8 +1,9 @@
-function togglePropositionsDisplay() {
+function togglePropositionsDisplay(e) {
 	var propositions = document.getElementsByClassName("proposition");
 	for (index = 0; index < propositions.length; ++index) {
 		propositions[index].style.display = (propositions[index].style.display === 'none' || propositions[index].style.display === '') ? 'block' : 'none';
 	}
+	e.preventDefault();
 }
 
 document.addEventListener('DOMContentLoaded', function(){
